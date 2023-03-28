3647e78dab6f95922886be612219c8e9439609bf
Allan Sousa <allansousa.dev@gmail.com>
2023-03-28 19:34:01 -0300

    Inciando com fundamentos no JS

---
+++ fundamentos-js/main.js
@@ -0,0 +1,51 @@
+// let firstName = "Alan"
+// console.log(firstName)
+
+
+// console.error("Este é um erro")
+// console.warn("Este é um warn")
+// console.log("Este é um texto")
+
+// let x = 5
+// let y = 10 
+// let z = 15.5
+
+// if (1) {
+//     let x = 15
+//     console.log(x)
+// }
+
+// console.log(x, y, z)
+
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof z)
+
+// const decimal = 12.4
+// console.log(decimal)
+// console.log(typeof decimal)
+
+// const isApproved = false
+// console.log(isApproved)
+// console.log(typeof isApproved)
+
+// let surName = null
+// console.log(surName)
+// console.log(typeof surName)
+
+// let age = 20
+// console.log(age)
+// console.log(typeof age)
+
+
+// const language = ["Javascript", "Rust", "GO", "PHP"]
+// console.log(language)
+// console.log(typeof language)
+
+// const user = {Name: "Afonso", Email:"afonso@gmail.com"}
+// console.log(user)
+// console.log(typeof user)
+
+const fullName = "Maria de Sá"
+console.log(fullName.length)
+console.log(typeof fullName)
