8bf70fe97e1895e1dba1cc9f2737790436a683e3
Allan Sousa <allansousa.dev@gmail.com>
2023-05-08 18:48:17 -0300

    Alterações realizadas nos arquivos main.js, index.html e anotações.md

---
+++ discover/anotacoes.md
@@ -0,0 +1,8 @@
+
+Sem executar o código abaixo, diga qual será a resposta informada
+
+```javascript
+    console.log(a)
+    var a = 1
+```
+A resposta será um undefined pois a variável a será elevada sofrendo o processo de hosting então ela é identificada mas não possui um valor determinado.
--- discover/main.js
+++ discover/main.js
@@ -1,1 +1,79 @@
-console.log('Hello world')
+// const person = {
+//     nameMen: 'Jonh',
+//     age: 34,
+//     weight: 88.7,
+//     isAdmin: true
+// }
+
+// console.log(person.nameMen)
+// console.log(person.age)
+// console.log(person.weight)
+// console.log(person.isAdmin)
+
+// console.log(`${person.nameMen} tem ${person.age} de idade, e pesa ${person.weight}kg`)
+
+// // Array
+// const animals = [
+//     'Lion',
+//     'Monkey',
+//     {
+//         name: 'cat',
+//         age: 3
+//     }
+
+// ]
+
+
+// // Acessar valores dos arrays
+// console.log(animals[0])
+// console.log(animals.length)
+// console.log(animals[2].name)
+
+// let weight
+
+// console.log(typeof weight)
+// // Tipo de dado undefined
+
+// let primaryName =  'Amanda'
+// let age = 15
+// let stars = 5.3
+// let isSubscribed = true
+
+// console.log(primaryName, age, stars, isSubscribed)
+
+// let student = {}
+// console.log(typeof student)
+// // Type object
+
+const primaryStudent = {
+    name: "Amanda",
+    age: 15,
+    stars: 5.3,
+    isSubscribed: true,
+    weight: 65.3
+}
+
+// console.log(`${primaryStudent.name} de ${primaryStudent.age} anos de idade pesa ${primaryStudent.weight}kg`)
+const larissa = {
+    name: "Larissa",
+    age: 18,
+    stars: 9.7,
+    isSubscribed: true,
+    weight: 78.9
+}
+
+const students = [
+        primaryStudent,
+        larissa
+    ]
+    
+console.log(students)
+console.log(students[0])
+console.log(typeof (students[0]))
+console.log(students[1])
+console.log(typeof students[1])
+console.log(students)
+
+// undefined 
+console.log(a)
+var a = 1
