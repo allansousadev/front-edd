283b65d231db38831467c4d1c3f1f4dfe9c3908d
Allan Sousa <allansousa.dev@gmail.com>
2023-05-29 17:12:42 -0300

    Fim dos estudos com comdicionais e controle de fluxo da aplicação

--- discover/condicionais_e_controle_de_fluxo/main.js
+++ discover/condicionais_e_controle_de_fluxo/main.js
@@ -24,3 +24,94 @@ document.querySelector('#app').innerHTML = `
 setupCounter(document.querySelector('#counter'))
 
 console.log('hello world')
+
+// Control Flow
+// Fluxo da aplicação
+
+console.log('a')
+console.log('b')
+
+
+// if....else
+
+let temperature = 36.5
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37.5 && temperature >=  37
+
+// if(temperature >= 37.5) {
+//   console.log('Febre alta')
+// } else if(temperature < 37.5 && temperature >= 37) {
+//   console.log('Febre moderada')
+// } else {
+//   console.log('Saudável')
+// }
+
+// Outra forma de escrita do loop
+if(highTemperature) {
+  console.log('Febre alta')
+} else if(mediumTemperature) {
+  console.log('Febre moderada')
+} else {
+  console.log('Saudável')
+}
+
+// switch
+
+let expression = 'c'
+
+switch (expression) {
+  case 'a':
+    console.log('Oi')
+    break
+  case 'b':
+    console.log('Hello')
+    break
+  default:
+    console.log('default')
+    break
+}
+
+
+function calculate(number1, operator, number2) {
+  let result = 0
+
+  switch (operator) {
+    case '+':
+      result = number1 + number2
+      break
+    case '-':
+      result = number1 - number2
+      break
+    case '*':
+      result = number1 * number2
+      break
+    case '/':
+      result = number1 / number2
+      break
+    default:
+        console.log('Não implementada')
+        break
+}
+
+return result
+}
+
+console.log(calculate(2, '%', 3))
+
+
+// throw (lançar)
+function sayMyName(name = '') {
+  if (name === '') {
+    throw 'Nome é obrigatório'
+  }
+
+  console.log(name)
+}
+
+
+// try...catch (pegar)
+try {
+  sayMyName('Julia')
+} catch(e) {
+  console.log(e)
+}
